07f816ff6e0419f9af98c90d9776fd6dbe7001ab
gayusishaku <83911941+gayusishaku@users.noreply.github.com>
2021-07-17 20:20:35 +0100

    Add files via upload

---
+++ send.php
@@ -0,0 +1,17 @@
+<?php
+include("connectchat.php");
+
+$msg = $_REQUEST["txtarea"];
+
+session_start();
+$userid = $_SESSION["userid"];
+$date = date("g:i a");
+
+$send = mysqli_query($conn,"INSERT INTO chattb VALUES('','$userid','$msg','$date') ");
+
+if($send)
+{
+	header("Location: homechat.php");
+}
+
+?>
